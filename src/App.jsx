e1b6c5b7f0210902a93cd6172a53f227ed0256c5
alvaaguero <agueroalvaro010@gmail.com>
2023-06-11 23:23:18 -0300

    productos

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import ItemDetailContainer from "./components/ItemDetailContainer";
+import ItemListContainer from "./components/ItemListContainer";
+import Navbar from "./components/Navbar";
 import "./main.css"
 
 function App() {
+
   return (
     <div>
-        hola mundo
+      <Navbar />
+      <ItemListContainer />
+      <ItemDetailContainer itemId={2} />
     </div>
   );
 }
